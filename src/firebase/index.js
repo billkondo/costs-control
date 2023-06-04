93bc9394758ff9f5a5c4da7c0d5711fd26b9b0f3
William Hideki Kondo <willkondo@gmail.com>
2023-06-04 19:12:30 -0300

    Setup Firebase Authentication

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
 import firebaseConfig from './firebaseConfig';
 
@@ -8,13 +9,17 @@ let app;
 /** @type {import('firebase/firestore').Firestore} */
 let db;
 
+/** @type {import('firebase/auth').Auth} */
+let auth;
+
 const init = () => {
   app = initializeApp(firebaseConfig);
   db = getFirestore(app);
+  auth = getAuth(app);
 
   if (import.meta.env.MODE === 'development') {
     connectFirestoreEmulator(db, 'localhost', 8080);
   }
 };
 
-export { init, app, db };
+export { init, app, db, auth };
