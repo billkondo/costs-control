b33e8c834e93c4ab7eb3ad30d5793ff0f17e8396
William Hideki Kondo <willkondo@gmail.com>
2023-09-24 14:31:21 -0300

    Improve monthly expenses chart

--- src/components/MonthlyExpensesChart.jsx
+++ src/components/MonthlyExpensesChart.jsx
@@ -20,11 +20,16 @@ const MonthlyExpensesChart = (props) => {
 
   const xLabels = monthlyExpenses.map(getChartLabel);
   const yData = monthlyExpenses.map(({ value }) => value);
+  const allZeros = yData.reduce(
+    (allZeros, value) => allZeros && value === 0,
+    true
+  );
 
   return (
     <LineChart
       xAxis={[
         {
+          id: 'months',
           data: xLabels,
           scaleType: 'band',
         },
@@ -37,12 +42,21 @@ const MonthlyExpensesChart = (props) => {
           valueFormatter: brlFormat,
         },
       ]}
+      yAxis={[
+        {
+          max: allZeros ? 1000 : undefined,
+        },
+      ]}
       height={300}
       margin={{
         top: 40,
         left: 40,
         right: 0,
         bottom: 40,
+      }}
+      bottomAxis={{
+        axisId: 'months',
+        disableTicks: true,
       }}
       sx={{
         width: '100%',
